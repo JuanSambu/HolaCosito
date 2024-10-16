d29c93a4c8f98463c25fa6dcab8f08501f25d526
Juan <jsambucetti@itba.edu.ar>
2024-10-16 10:39:27 -0300

    Primer push, incluye main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 16, 2024
+ *      Author: juan
+ */
+
+#include <stdio.h>
+
+int main (void) {
+	printf("Control tower this is Eclipse talking\n");
+	return 0;
+}
